1bbae27a6c7894f8fc24a7d14e41c3e44620dd9a
Jacek Królikowski <nietaki@gmail.com>
2026-01-07 03:02:43 +0100

    Less logspam

--- convert_all.sh
+++ convert_all.sh
@@ -5,10 +5,23 @@ INPUT_DIR="/input"
 OUTPUT_DIR="/output"
 BITRATE=224
 
-echo "we're in"
+FILE_COUNT=$(find "$INPUT_DIR" -type f -size +100k -name "*.flac" | wc -l)
+echo "Found $FILE_COUNT FLAC files to convert."
+
+FILE_NO=0
+
 
 find "$INPUT_DIR" -type f -size +100k -name "*.flac" | while read -r FLAC_FILE; do
     # Determine the relative path of the FLAC file with respect to the input directory
+
+    FILE_NO=$((FILE_NO + 1))
+    # every 100 files, print progress
+    if (( FILE_NO % 100 == 0 )); then
+        PERCENT=$(( FILE_NO * 100 / FILE_COUNT ))
+        echo "$PERCENT%: $FILE_NO / $FILE_COUNT files."
+        # print progress percentage
+    fi
+
     RELATIVE_PATH="${FLAC_FILE#$INPUT_DIR/}"
     
     # Determine the output directory and create it if it doesn't exist
@@ -20,11 +33,11 @@ find "$INPUT_DIR" -type f -size +100k -name "*.flac" | while read -r FLAC_FILE; 
 
     # check if the file already exists
     if [ -f "$OUTPUT_FILE" ]; then
-        echo "Skipping '$FLAC_FILE' as '$OUTPUT_FILE' already exists."
+        # echo "Skipping '$FLAC_FILE' as '$OUTPUT_FILE' already exists."
         continue
     fi
 
-    echo "Converting '$FLAC_FILE' to '$OUTPUT_FILE'"
+    echo "\nConverting '$FLAC_FILE'"
     
     # Convert the FLAC file to OPUS format using opusenc
     opusenc --no-phase-inv --downmix-stereo --bitrate "$BITRATE" "$FLAC_FILE" "$OUTPUT_FILE"
